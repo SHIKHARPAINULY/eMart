8c8733d4756631f4f39da79fcb56c93cf6d08d32
shikhar <shikharpainuly@torum.com>
2022-04-04 12:06:35 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './modules/home';
+import { Routes, Route } from 'react-router-dom';
+import Seller from './modules/seller';
+import Buyer from './modules/buyer';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="seller" element={<Seller />} />
+        <Route path="buyer" element={<Buyer />} />
+      </Routes>
     </div>
   );
 }
